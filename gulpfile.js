96c927aca04f6d77fd5a6506ff818388a9cdc14c
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-01-19 21:59:18 +0300

    gulp2

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+function style () {
+    return gulp.src('./scss/**/*.scss').pipe
+            .pipe(sass())
+            .pipe(gulp.dest('/.css'))
+}
+
+export.style = style;
