934a1de286bfb9ebda9114da6ab1f38c2ad7c32f
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-01-20 00:22:14 +0300

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,19 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-function style () {
+function style() {
     return gulp.src('./scss/**/*.scss')
-            .pipe(sass())
-            .pipe(gulp.dest('./css'))
-            .pipe(browserSync.stream())
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
 }
 
-    function watch () {
+function watch() {
     browserSync.init({
         server: {
             baseDir: './'
         }
-        })
-
-
-
+    })
     gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
